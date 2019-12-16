ec0d5b4e5ef9d1840af662b13778a7a9d81ab7f4
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2019-12-16 12:59:51 +0100

    supp variable inutile

--- Utils/tools.py
+++ Utils/tools.py
@@ -6,7 +6,6 @@ def open_file(filename):
     if os.path.exists(filename):
         with open(filename) as fp:
             line = fp.readline()
-            cnt = 1
             while line:
                 my_list.append(line)
                 line = fp.readline()
