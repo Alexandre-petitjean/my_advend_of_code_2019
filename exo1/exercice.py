b98e8a884d8e1a2661aec7a96abc8c251d0abb41
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2019-12-16 12:58:39 +0100

    exo 1 partie 2

--- exo1/exercice.py
+++ exo1/exercice.py
@@ -7,14 +7,34 @@ def exo1():
     print("Exercice 1")
     filename = "exo1/exo_1.txt"
     my_list = open_file(filename)
-    print(calcule(my_list))
+    # print(calculate_part_1(my_list))
+    print(calculate_part_2(my_list))
 
 
-def calcule(my_list):
+def calculate_part_1(my_list):
     result = 0
     for i in my_list:
-        calcul = int(i) / 3
-        calcul = math.floor(calcul)
-        calcul = calcul - 2
-        result += calcul
+        calc = fuel_require(int(i))
+        result += calc
     return result
+
+
+def calculate_part_2(my_list):
+    result = 0
+    for i in my_list:
+        calc = fuel_require(int(i))
+        result += calc
+        while calc > 0:
+            calc = fuel_require(calc)
+            result += calc
+    return result
+
+
+def fuel_require(i):
+    calc = i / 3
+    calc = math.floor(calc)
+    calc = calc - 2
+    if calc < 0:
+        calc = 0
+    return calc
+
--- exo1/exercice_test.py
+++ exo1/exercice_test.py
@@ -1,25 +1,37 @@
 import unittest
 
-from exo1.exercice import calcule
+from exo1.exercice import calculate_part_1, calculate_part_2
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_calcule_12(self):
+    def test_calcule_part1_12(self):
         my_list = [12]
-        self.assertEqual(calcule(my_list), 2, "KO")
+        self.assertEqual(calculate_part_1(my_list), 2, "KO")
 
-    def test_calcule_14(self):
+    def test_calcule_part1_14(self):
         my_list = [14]
-        self.assertEqual(calcule(my_list), 2, "KO")
+        self.assertEqual(calculate_part_1(my_list), 2, "KO")
 
-    def test_calcule_1969(self):
+    def test_calcule_part1_1969(self):
         my_list = [1969]
-        self.assertEqual(calcule(my_list), 654, "KO")
+        self.assertEqual(calculate_part_1(my_list), 654, "KO")
 
-    def test_calcule_100756(self):
+    def test_calcule_part1_100756(self):
         my_list = [100756]
-        self.assertEqual(calcule(my_list), 33583, "KO")
+        self.assertEqual(calculate_part_1(my_list), 33583, "KO")
+
+    def test_calcule_part2_14(self):
+        my_list = [14]
+        self.assertEqual(calculate_part_2(my_list), 2, "KO")
+
+    def test_calcule_part2_1969(self):
+        my_list = [1969]
+        self.assertEqual(calculate_part_2(my_list), 966, "KO")
+
+    def test_calcule_part2_100756(self):
+        my_list = [100756]
+        self.assertEqual(calculate_part_2(my_list), 50346, "KO")
 
 
 if __name__ == '__main__':
