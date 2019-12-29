a4409242b84b7bb91050aef45904dc03ef6f7b4d
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2019-12-29 13:28:07 +0100

    ajout de test unitaire

--- jour4/exercice.py
+++ jour4/exercice.py
@@ -5,39 +5,44 @@ def exercice_4():
     print("Exercice 4")
     filename = "jour4/jour4.txt"
     my_list = open_file_line_by_line(filename)
-    result = treatment(my_list)
+    borne = setup(my_list)
+    result = treatment(borne[0], borne[1])
     print(result)
 
 
-def treatment(my_list):
-    result_list = []
+def setup(my_list):
     borne = my_list[0].split("-")
-    borne_min = borne[0]
-    borne_max = borne[1]
-    i = int(borne_min)
-    while i <= int(borne_max):
-        if same_adj_digit(i) and digit_never_decrease(i):
-            result_list.append(i)
-        i = i + 1
+    return borne
+
+
+def treatment(borne_min, borne_max):
+    result_list = []
+
+    number = int(borne_min)
+    while number <= int(borne_max):
+        if same_adj_digit(number) and digit_never_decrease(number):
+            result_list.append(number)
+        number = number + 1
+
     return result_list.__len__()
 
 
-def same_adj_digit(i):
+def same_adj_digit(number):
     result = False
-    string = str(i)
-    for i in range(string.__len__()):
-        if i < string.__len__() - 1:
-            if string[i] == string[i + 1]:
+    string = str(number)
+    for number in range(string.__len__()):
+        if number < string.__len__() - 1:
+            if string[number] == string[number + 1]:
                 result = True
     return result
 
 
-def digit_never_decrease(i):
+def digit_never_decrease(number):
     result = True
-    string = str(i)
-    i = 1
-    while i < string.__len__():
-        if string[i] < string[i - 1]:
+    string = str(number)
+    number = 1
+    while number < string.__len__():
+        if string[number] < string[number - 1]:
             result = False
-        i = i + 1
+        number = number + 1
     return result
--- jour4/exercice_test.py
+++ jour4/exercice_test.py
@@ -1,9 +1,25 @@
 import unittest
 
+from jour4.exercice import digit_never_decrease, same_adj_digit
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    borne_min = 273025
+    borne_max = 767253
+
+    def test_decreasing_number(self):
+        my_number = 223450
+        self.assertEqual(digit_never_decrease(my_number), False)
+
+    def test_no_double_number(self):
+        my_number = 123789
+        self.assertEqual(same_adj_digit(my_number), False)
+
+    def test_good_number(self):
+        my_number = 111111
+        self.assertEqual(same_adj_digit(my_number), True)
+        self.assertEqual(digit_never_decrease(my_number), True)
 
 
 if __name__ == '__main__':
