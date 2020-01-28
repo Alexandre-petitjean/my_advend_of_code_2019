f5ababef135b384151d4595c96ef2b4249d4e7e4
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2020-01-28 19:23:38 +0100

    fin du jour 5

--- jour5/exercice.py
+++ jour5/exercice.py
@@ -9,6 +9,7 @@ JUMPT = '5'
 JUMPF = '6'
 LESS = '7'
 EQUALS = '8'
+STOP = 99
 
 
 def exercice_5():
@@ -19,7 +20,8 @@ def exercice_5():
     filename = "jour5/jour5.txt"
     my_list = open_file_explode_array(filename)
     list_str_to_int(my_list)
-    treatment_part_1(my_list)
+    # treatment_part_1(my_list)
+    treatment_part_2(my_list)
 
 
 def list_str_to_int(my_list):
@@ -51,7 +53,53 @@ def treatment_part_1(my_list):
     return treatment(my_list, my_input)
 
 
-def op_code_parametrer_mode(mode1, mode2, opcode, i, my_list):
+def treatment_part_2(my_list):
+    """
+    Traitement de la partie 2 du jour.
+    :param my_list:
+    :return:
+    """
+    my_input = 5
+    return treatment(my_list, my_input)
+
+
+def treatment(my_list, my_input):
+    """
+    Traitement pour effectuer la résolution de l'exercice.
+    :param my_list: La liste de int.
+    :param my_input: L'input demandé pour le opcode 3.
+    """
+    i = 0
+    result = 0
+    while i < my_list.__len__() and my_list[i] != STOP:
+        number = str(my_list[i])
+        # Tous les opcodes doivent être des nombres à 5 chiffres.
+        while len(number) < 5:
+            number = '0' + number
+
+        mode2 = number[1]
+        mode1 = number[2]
+        opcode = number[4]
+
+        if opcode == ADD or opcode == MULT:
+            op_code_add_mult(mode1, mode2, opcode, i, my_list)
+            i = i + 4
+        elif opcode == LESS or opcode == EQUALS:
+            op_code_less_equals(mode1, mode2, opcode, i, my_list)
+            i = i + 4
+        elif opcode == IN or opcode == OUT:
+            if opcode == IN:
+                op_code_in(i, my_list, my_input)
+            elif opcode == OUT:
+                result = op_code_out(i, my_list)
+            i = i + 2
+        elif opcode == JUMPT or opcode == JUMPF:
+            i = op_code_jumpt_jumf(mode1, mode2, opcode, i, my_list)
+
+    return result
+
+
+def op_code_add_mult(mode1, mode2, opcode, i, my_list):
     index = get_index(i, my_list)
 
     if mode1 == '1':
@@ -69,33 +117,6 @@ def op_code_parametrer_mode(mode1, mode2, opcode, i, my_list):
         calc = param1 * param2
 
     my_list[index[2]] = calc
-
-
-def treatment(my_list, my_input):
-    """
-    Traitement pour effectuer la résolution de l'exercice.
-    :param my_list: La liste de int.
-    :param my_input: L'input demandé pour le opcode 3.
-    """
-    i = 0
-    while i < my_list.__len__():
-        number = str(my_list[i])
-        # Tous les opcode doivent être des nombre à 5 chiffres.
-        while len(number) < 5:
-            number = '0' + number
-        mode2 = number[1]
-        mode1 = number[2]
-        opcode = number[4]
-
-        if opcode == ADD or opcode == MULT:
-            op_code_parametrer_mode(mode1, mode2, opcode, i, my_list)
-            i = i + 4
-        elif opcode == IN or opcode == OUT:
-            if opcode == IN:
-                op_code_in(i, my_list, my_input)
-            elif opcode == OUT:
-                op_code_out(i, my_list)
-            i = i + 2
 
 
 def op_code_in(i, my_list, my_input):
@@ -117,3 +138,51 @@ def op_code_out(i, my_list):
     """
     target = my_list[i + 1]
     print(str(i) + " - " + str(my_list[target]))
+    return my_list[target]
+
+
+def op_code_jumpt_jumf(mode1, mode2,opcode, i, my_list):
+    index = get_index(i, my_list)
+    if mode1 == '1':
+        param1 = index[0]
+    else:
+        param1 = my_list[index[0]]
+    if mode2 == '1':
+        param2 = index[1]
+    else:
+        param2 = my_list[index[1]]
+
+    if opcode == JUMPT:
+        if param1 != 0:
+            i = param2
+        else:
+            i = i + 3
+    else:
+        if param1 == 0:
+            i = param2
+        else:
+            i = i + 3
+    return i
+
+
+def op_code_less_equals(mode1, mode2, opcode, i, my_list):
+    index = get_index(i, my_list)
+    result = 0
+
+    if mode1 == '1':
+        param1 = index[0]
+    else:
+        param1 = my_list[index[0]]
+    if mode2 == '1':
+        param2 = index[1]
+    else:
+        param2 = my_list[index[1]]
+
+    if opcode == LESS:
+        if param1 < param2:
+            result = 1
+    else:
+        if param1 == param2:
+            result = 1
+
+    my_list[index[2]] = result
