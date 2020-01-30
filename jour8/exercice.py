5ddb45dc3082290b03e226a480bb39b4aa35cf59
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2020-01-30 18:08:47 +0100

    partie 2 non terminée

--- jour8/exercice.py
+++ jour8/exercice.py
@@ -1,4 +1,5 @@
 from textwrap import wrap
+from PIL import Image, ImageColor
 
 import Utils.tools
 
@@ -10,7 +11,14 @@ def exercice_8():
     print("Jour 8")
     filename = "jour8/jour8.txt"
     my_list = Utils.tools.open_file_line_by_line(filename)
-    treatment_part_1(my_list)
+
+    wide = 25
+    tall = 6
+    size = wide * tall
+    my_list = explode_size(my_list, size)
+
+    # treatment_part_1(my_list)
+    treatment_part_2(my_list)
 
 
 def explode_size(my_list, size):
@@ -28,18 +36,6 @@ def treatment_part_1(my_list):
     Fonction qui sert au traitement de la partie 1.
     :param my_list: La liste des données à traiter.
     """
-    wide = 25
-    tall = 6
-    size = wide * tall
-    my_list = explode_size(my_list, size)
-    treatment(my_list)
-
-
-def treatment(my_list):
-    """
-    Fonction qui effectue le traitement principale.
-    :param my_list: La liste des données à traiter.
-    """
     result = my_list[0]
     i = 1
     while i < my_list.__len__():
@@ -50,6 +46,47 @@ def treatment(my_list):
     nb_one = count_digit(result, '1')
     nb_two = count_digit(result, '2')
     print(nb_one * nb_two)
+
+
+def treatment_part_2(my_list):
+    """
+    Fonction qui sert au traitement de la partie 1.
+    :param my_list: La liste des données à traiter.
+    """
+    new_layer = []
+    i = 0
+    while i < len(my_list[0]):
+        j = 0
+        layer = ""
+        while j < my_list.__len__():
+            layer += my_list[j][i]
+            j = j + 1
+        new_layer.append(layer)
+        i = i + 1
+    layer = decode(new_layer)
+    draw(layer)
+
+
+def decode(new_layer):
+    final_layer = ""
+    for element in new_layer:
+        i = 0
+        while element[i] == '2':
+            i = i + 1
+        final_layer += element[i]
+    return final_layer
+
+
+def draw(layer):
+    im = Image.new('1', (25, 6))
+    layer = wrap(layer, 25)
+    for element in layer:
+        for pixel in element:
+            if pixel == '1':
+                im.putpixel((layer.index(element), element.index(pixel)), ImageColor.getcolor('black', '1'))
+            elif pixel == '0':
+                im.putpixel((layer.index(element), element.index(pixel)), ImageColor.getcolor('white', '1'))
+    im.show()
 
 
 def count_digit(element, digit):
---
+++ jour8/exercice_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from jour8.exercice import explode_size, treatment_part_2
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_part2(self):
+        my_list = ["0222112222120000"]
+
+        wide = 2
+        tall = 2
+        size = wide * tall
+        my_list = explode_size(my_list, size)
+        self.assertEqual(treatment_part_2(my_list))
+
+
+if __name__ == '__main__':
+    unittest.main()
