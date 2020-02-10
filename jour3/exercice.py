256ca3e31fc5b112f6083c401303a2cc569fa7eb
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2020-02-10 15:12:45 +0100

    day 3 part 2 DONE

--- jour3/exercice.py
+++ jour3/exercice.py
@@ -1,11 +1,11 @@
 """
 Day 3 of the avent of code.
 https://adventofcode.com/2019/day/3
-Probleme : Two wires cross each other, we need to find the closest intersection of wire.
+Problem : Two wires cross each other, we need to find the closest intersection of wire.
 """
 from Utils.tools import open_file_explode_array_line_by_line
 
-# Const
+# Constant
 RIGHT = 'R'
 LEFT = 'L'
 UP = 'U'
@@ -16,7 +16,59 @@ def exercice_3():
     print("Exercice 3")
     filename = "jour3/exo_3.txt"
     my_list = open_file_explode_array_line_by_line(filename)
-    print(treatement(my_list))
+    # print(treatment_part1(my_list))
+    print(treatment_part2(my_list))
+
+
+def treatment_part1(my_list):
+    """
+    Treatement of the part 1.
+    :param my_list: The input.
+    :return: The part 1 answer.
+    """
+    wire_path = treatment(my_list)
+    intersection = find_intersection(wire_path)
+    return manhattan_calcul(intersection)
+
+
+def treatment_part2(my_list):
+    """
+    Treatment of the part 2.
+    :param my_list: The input.
+    :return: The part 2 answer.
+    """
+    wires_path = treatment(my_list)
+    intersection = find_intersection(wires_path)
+    return fewest_step(wires_path, intersection)
+
+
+def treatment(my_list):
+    """
+    Principal treatment.
+    :param my_list: The input.
+    :return: The path of the two wires.
+    """
+    wires_path = [[], []]
+    for wire in my_list:
+        x = 0
+        y = 0
+        one_wire_path = []
+        for element in wire:
+            # Direction du fil.
+            first_letter = element[0]
+            # Distance a parcourir.
+            iteration = int(element[1:])
+            one_wire_path = calcul_path(first_letter, iteration, x, y, one_wire_path)
+            if first_letter == RIGHT:
+                x = x + iteration
+            elif first_letter == LEFT:
+                x = x - iteration
+            elif first_letter == UP:
+                y = y + iteration
+            else:
+                y = y - iteration
+        wires_path[my_list.index(wire)] = one_wire_path
+    return wires_path
 
 
 def calcul_path(direction, iteration, x, y, one_wire_path):
@@ -61,7 +113,12 @@ def calcul_path(direction, iteration, x, y, one_wire_path):
     return one_wire_path
 
 
-def count_croisement(wire_path):
+def find_intersection(wire_path):
+    """
+    Find the coordinate of intersection of the two wire.
+    :param wire_path: List of coordinate of the wires path.
+    :return: The list of intersection coordinate.
+    """
     # On retire le premier element de chaque list qui est 0;0
     first_wire = wire_path[0][1:]
     second_wire = wire_path[1][1:]
@@ -70,9 +127,15 @@ def count_croisement(wire_path):
     return set(first_wire).intersection(second_wire)
 
 
-def manhattan_calcul(croisement):
+def manhattan_calcul(intersection):
+    """
+    Calcul of Manhattan distance.
+    https://en.wikipedia.org/wiki/Taxicab_geometry
+    :param intersection: List of coordinate.
+    :return: The manhattan distance of the shortest intersection point.
+    """
     result: int = None
-    for element in croisement:
+    for element in intersection:
         position = element.split(";")
         x = abs(int(position[0]))
         y = abs(int(position[1]))
@@ -82,28 +145,20 @@ def manhattan_calcul(croisement):
     return result
 
 
-def treatement(my_list):
-    wire_path = [[], []]
-    for wire in my_list:
-        x = 0
-        y = 0
-        one_wire_path = []
-        for element in wire:
-            # Direction du fil.
-            first_letter = element[0]
-            # Distance a parcourir.
-            iteration = int(element[1:])
-            one_wire_path = calcul_path(first_letter, iteration, x, y, one_wire_path)
-            if first_letter == RIGHT:
-                x = x + iteration
-            elif first_letter == LEFT:
-                x = x - iteration
-            elif first_letter == UP:
-                y = y + iteration
-            else:
-                y = y - iteration
-        wire_path[my_list.index(wire)] = one_wire_path
-
-    croisement = count_croisement(wire_path)
-    print(croisement)
-    return manhattan_calcul(croisement)
+def fewest_step(wires_path, intersections):
+    """
+    Calcul the fewest step to an intersection.
+    :param wires_path: the path of the wires.
+    :param intersections: the coordinate of the intersections.
+    """
+    result: int = None
+    for coordinate in intersections:
+        step = 0
+        for path in wires_path:
+            i = 0
+            while coordinate != path[i]:
+                i = i + 1
+            step += i
+        if result is None or result > step:
+            result = step
+    return result
--- jour3/exercice_test.py
+++ jour3/exercice_test.py
@@ -1,23 +1,37 @@
 import unittest
 
-from jour3.exercice import treatement
+from jour3.exercice import treatment_part1, treatment_part2
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_1(self):
+    def test_part1_1(self):
         my_list = [["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"]]
-        self.assertEqual(treatement(my_list), 6, True)
+        self.assertEqual(treatment_part1(my_list), 6, True)
 
-    def test_2(self):
+    def test_part1_2(self):
         my_list = [["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
                    ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]]
-        self.assertEqual(treatement(my_list), 159, True)
+        self.assertEqual(treatment_part1(my_list), 159, True)
 
-    def test_3(self):
+    def test_part1_3(self):
         my_list = [["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"],
                    ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]]
-        self.assertEqual(treatement(my_list), 135, True)
+        self.assertEqual(treatment_part1(my_list), 135, True)
+
+    def test_part2_1(self):
+        my_list = [["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"]]
+        self.assertEqual(treatment_part2(my_list), 30, True)
+
+    def test_part2_2(self):
+        my_list = [["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
+                   ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]]
+        self.assertEqual(treatment_part2(my_list), 610, True)
+
+    def test_part2_3(self):
+        my_list = [["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"],
+                   ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]]
+        self.assertEqual(treatment_part2(my_list), 410, True)
 
 
 if __name__ == '__main__':
