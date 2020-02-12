87c5e5eec2b6d90c9e6234a17f393fcd41da63ab
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2020-02-12 17:30:07 +0100

    day 6 part 2 DONE

--- jour6/Tree/Tree.py
+++ jour6/Tree/Tree.py
@@ -1,5 +1,4 @@
 class Tree:
-
     lastNode = None
     node_list = []
 
@@ -30,6 +29,58 @@ class Tree:
         for child in node.children:
             my_string = self.course_tree(my_string, child)
         return my_string
+
+    @staticmethod
+    def get_ancestor_list(ancestor_list, node):
+        """
+        Get the ancestor list of the node.
+        @param ancestor_list: 
+        @param node: 
+        @return: 
+        """
+        while node.parent is not None:
+            ancestor_list.append(node.parent)
+            node = node.parent
+        return ancestor_list
+
+    @staticmethod
+    def get_first_parent(ancestors_node1, ancestors_node2):
+        """
+        Get the first commun parent.
+        @param ancestors_node1: The list of parents of the node 1.
+        @param ancestors_node2: The list of parents of the node 2.
+        @return: The first commun parent.
+        """
+        parent = None
+        i = 0
+        while i < ancestors_node1.__len__() and parent is None:
+            j = 0
+            while j < ancestors_node2.__len__() and parent is None:
+                if ancestors_node1[i].get_name() == ancestors_node2[j].get_name():
+                    parent = ancestors_node1[i]
+                j = j + 1
+            i = i + 1
+        return parent
+
+    @staticmethod
+    def get_distance_for_first_parent(parent_list, target_parent):
+        result = 0
+        i = 0
+        while i < parent_list.__len__() and result == 0:
+            if parent_list[i] == target_parent:
+                result = i
+            i = i + 1
+        return result
+
+    def get_distance_between_node(self, node1, node2):
+        parents_node1 = []
+        parents_node2 = []
+        parents_node1 = self.get_ancestor_list(parents_node1, node1)
+        parents_node2 = self.get_ancestor_list(parents_node2, node2)
+        parent = self.get_first_parent(parents_node1, parents_node2)
+        distance_node1 = self.get_distance_for_first_parent(parents_node1, parent)
+        distance_node2 = self.get_distance_for_first_parent(parents_node2, parent)
+        return distance_node1 + distance_node2
 
     def __str__(self):
         node = self.root
--- jour6/exercice.py
+++ jour6/exercice.py
@@ -8,21 +8,51 @@ def day_6():
     filename = "jour6/jour_6.txt"
     my_list = open_file_line_by_line(filename)
     # my_list = ["COM)B", "B)C", "C)D", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "D)E", "K)L"]
-    treatment_part_1(my_list)
+    # treatment_part_1(my_list)
+    treatment_part_2(my_list)
 
 
 def treatment_part_1(my_list):
     root_name = "COM"
     tree = make_tree(my_list, root_name)
-    print(treatment(my_list, tree))
+    tree = treatment(my_list, tree)
+    orbit_sum = calcul_orbit(tree.get_root(), 0)
+    print(orbit_sum)
+
+
+def treatment_part_2(my_list):
+    root_name = "COM"
+    tree = make_tree(my_list, root_name)
+    tree = treatment(my_list, tree)
+    print(orbital_transfers(tree, "YOU", "SAN"))
+
+
+def treatment(my_list, tree):
+    """
+    This is the main treatment of the day.
+    @rtype: Tree
+    """
+    while my_list.__len__() > 0:
+        rows = find_rows(my_list, Tree.lastNode.get_name())
+        add_nodes(tree, rows)
+    return tree
 
 
 def delete_rows(my_list, result):
+    """
+    Delete the rows for the list in parameter.
+    @param my_list: The input list.
+    @param result: the rows to be deleted.
+    """
     for element in result:
         my_list.remove(element)
 
 
 def find_rows(my_list, nom_node):
+    """
+    Find rows witch containe a specific node name.
+    @rtype: A array.
+    """
     result = [i for i in my_list if nom_node in i]
     if result.__len__() == 0:
         j = 0
@@ -49,8 +79,12 @@ def make_tree(my_list, root_name):
 
 
 def add_nodes(tree, rows):
+    """
+    Add a node to the tree.
+    @param tree: The tree object.
+    @param rows: A row in the input file.
+    """
     for row in rows:
-
         obj = row.split(")")
         if obj[0] == tree.lastNode.get_name():
             parent = tree.lastNode
@@ -59,17 +93,13 @@ def add_nodes(tree, rows):
         Node(obj[1], parent)
 
 
-def get_row(my_list, tree):
-    i = 0
-    result = None
-    while i < my_list.__len__() and result is None:
-        if tree.find_node_by_name(tree.get_root(), my_list[i].split(")")[0]):
-            result = my_list[i]
-        i = i + 1
-    return [result]
-
-
 def calcul_orbit(node, orbit):
+    """
+    Calculation of the sum of direct and indirect orbits.
+    @param node: An Node object.
+    @param orbit: The sum of direct and indirect orbits.
+    @return: The sum of direct and indirect orbits.
+    """
     if node.depth != 0:
         orbit += node.get_depth() - 1
     for child in node.children:
@@ -78,9 +108,12 @@ def calcul_orbit(node, orbit):
     return orbit
 
 
-def treatment(my_list, tree):
-    while my_list.__len__() > 0:
-        rows = find_rows(my_list, Tree.lastNode.get_name())
-        add_nodes(tree, rows)
-    print(tree)
-    return calcul_orbit(tree.get_root(), 0)
+def orbital_transfers(tree, me_name, santa_name):
+    """
+    Calculation of the orbital transferes needed for join Santa.
+    @rtype: The number of orbital transferes.
+    """
+    me = tree.find_node_by_name(tree.get_root(), me_name)
+    santa = tree.find_node_by_name(tree.get_root(), santa_name)
+    distance = tree.get_distance_between_node(me, santa)
+    return distance
