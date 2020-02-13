426faf4ed3ad4c0892e95bfdee74af3fd7275de8
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2020-02-13 06:24:47 +0100

    day 7

---
+++ jour7/exercice.py
@@ -0,0 +1,12 @@
+from Utils.tools import open_file_explode_array
+
+
+def day_7():
+    print("Exercise 7")
+    filename = "jour7/jour_7.txt"
+    my_list = open_file_explode_array(filename)
+    treatment(my_list)
+
+
+def treatment(my_list):
+    pass
