6d227dc7978f784d3214ec335ac4a14c99a51148
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2020-02-20 10:14:41 +0100

    Annotation day 1 + code cleanup

--- day1/day_1_test.py
+++ day1/day_1_test.py
@@ -1,35 +1,34 @@
+"""
+Test of the day 1 of the Advent of code
+--- The Tyranny of the Rocket Equation ---
+"""
 import unittest
 
-from jour1.jour1 import calculate_part_1, calculate_part_2
+from day1.day_1 import calculate_part_1, calculate_part_2
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_calculation_part1_12(self):
+    def test_calculation_part1(self):
         my_list = [12]
         self.assertEqual(calculate_part_1(my_list), 2, "KO")
 
-    def test_calculation_part1_14(self):
         my_list = [14]
         self.assertEqual(calculate_part_1(my_list), 2, "KO")
 
-    def test_calculation_part1_1969(self):
         my_list = [1969]
         self.assertEqual(calculate_part_1(my_list), 654, "KO")
 
-    def test_calculation_part1_100756(self):
         my_list = [100756]
         self.assertEqual(calculate_part_1(my_list), 33583, "KO")
 
-    def test_calculation_part2_14(self):
+    def test_calculation_part2(self):
         my_list = [14]
         self.assertEqual(calculate_part_2(my_list), 2, "KO")
 
-    def test_calculation_part2_1969(self):
         my_list = [1969]
         self.assertEqual(calculate_part_2(my_list), 966, "KO")
 
-    def test_calculation_part2_100756(self):
         my_list = [100756]
         self.assertEqual(calculate_part_2(my_list), 50346, "KO")
 
