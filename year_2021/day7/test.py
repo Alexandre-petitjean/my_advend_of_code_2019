4c53b06aa88fe15d3b43bf639299ab14955ecd03
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2022-02-16 00:16:57 +0100

    Day 7 part 2

--- year_2021/day7/test.py
+++ year_2021/day7/test.py
@@ -1,10 +1,13 @@
 import unittest
 
-from year_2021.day7.main import findBestPos
+from year_2021.day7.main import findBestPosPart1, findBestPosPart2
 
 
 class MyTestCase(unittest.TestCase):
     input_list = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
-    def testSamePos(self):
-        self.assertEqual(37, findBestPos(self.input_list))
+    def testSamePosPart1(self):
+        self.assertEqual(37, findBestPosPart1(self.input_list))
+
+    def testSamePosePart2(self):
+        self.assertEqual(168, findBestPosPart2(self.input_list))
